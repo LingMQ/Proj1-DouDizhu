35713f4ac19686fa0091ad3ee150804ce0ab11c5
Sikang Hu <hu.sik@husky.neu.edu>
2019-10-17 01:04:30 -0400

    add function for initializing a game

--- doudizhu/lib/doudizhu/game.ex
+++ doudizhu/lib/doudizhu/game.ex
@@ -2,11 +2,56 @@ defmodule Doudizhu.Game do
 
   def new do
     %{
-      :hands %{},
-      :last %{},
-      :last_player: nil,
-      :base 0,
+      players: %{},
+      hands: [],
+      last: [],
+      last_player: {},
+      base: 0,
     }
+  end
+  
+  @doc """
+  Add an user to current table if there is a seat, report :error otherwise.
+  """
+  def add_player(game, player) do
+    p = game[:players]
+    if (map_size(p) < 3) do
+      game = %{game | players: Map.put(p, player, map_size(p))}
+      {:ok, game}
+    else
+      {:error, game}
+    end
+  end
+  
+  def init_game(game) do
+    %{game | hands: deal_cards()} 
+  end
+  
+  def assign_lord(game, player) do
+    index = game[:players][player]
+    hands = game[:hands]
+    hands = merge(hands, index, 3)
+    Map.put(game, :hands, hands)
+  end
+  
+  defp merge(list, a, b) do 
+    l1 = Enum.at(list, a)
+    l2 = Enum.at(list, b)
+    List.replace_at(list, a, l1 ++ l2)
+  end
+  
+  defp deal_cards() do
+    new_cards([], 53) 
+    |> Enum.shuffle
+    |> Enum.chunk_every(17)
+  end
+  
+  defp new_cards(card, 0) do
+   [0 | card]
+  end
+  
+  defp new_cards(card, count) do
+   new_cards([count | card], count - 1)
   end
 
 end
--- doudizhu/test/doudizhu/doudizhu_test.exs
+++ doudizhu/test/doudizhu/doudizhu_test.exs
@@ -2,7 +2,56 @@ defmodule Doudizhu.DoudizhuTest do
   use ExUnit.Case
   import Doudizhu.Game
   
-  test "single hand" do
-    assert validate(game, user, ["A"])
+  setup do
+   {:ok, game: new()}
+  end
+  
+  # TODO anyway to use pipe
+  test "add player", context do
+    game = context[:game]
+    {:ok, game} = add_player(game, "Tom")
+    {:ok, game} = add_player(game, "Jerry")
+    {:ok, game} = add_player(game, "John")
+    {:error, game} = add_player(game, "Mary")
+    assert Enum.sort(Map.keys(game[:players])) 
+      == Enum.sort(["Tom", "Jerry", "John"])
+  end
+  
+  test "deal card", context do
+   hands = context[:game] 
+         |> init_game 
+         |> Map.get(:hands)
+         |> Enum.map(fn x -> Enum.uniq(x) end)
+   assert length(Enum.at(hands, 0)) == 17
+   assert length(Enum.at(hands, 1)) == 17
+   assert length(Enum.at(hands, 2)) == 17
+   assert length(Enum.at(hands, 3)) == 3
+   assert hands 
+          |> List.flatten 
+          |> Enum.uniq
+          |> length
+          |> (&(&1 == 54)).()
+  end
+  
+  test "assign landlord", context do
+    game = context[:game]
+    {:ok, game} = add_player(game, "1")
+    {:ok, game} = add_player(game, "2")
+    {:ok, game} = add_player(game, "3")
+    hands = 
+    game
+    |> init_game
+    |> assign_lord("1")
+    |> Map.get(:hands)
+    assert length(Enum.at(hands, 0)) == 20
+    assert length(Enum.at(hands, 1)) == 17
+    assert length(Enum.at(hands, 2)) == 17
+    assert length(Enum.at(hands, 3)) == 3
+    assert hands 
+          |> List.flatten 
+          |> Enum.uniq
+          |> length
+          |> (&(&1 == 54)).()
+    
   end
 end
