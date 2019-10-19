06ee42f8ef628b6f2da95d612d63cbfb49bf3bf2
Sikang Hu <hu.sik@husky.neu.edu>
2019-10-19 14:13:12 -0400

    add test

--- doudizhu/lib/doudizhu/game_rule.ex
+++ doudizhu/lib/doudizhu/game_rule.ex
@@ -1,33 +1,52 @@
 defmodule Doudizhu.Rule do
   @moduledoc """
-  This is a module that imposes the rule of fighting landlord.
+  This is a module that imposes the rule of fighting landlord. 
+  Including recognizing the type of cards played and determine whether 
+  a playing can conquer the other.
   """
-  
+  @doc """
+  Recognize the type of the cards as none if nothing played.
+  """
   def get_cat([]) do
     :none
   end
-
+  
+  @doc """
+  Recognize the type of the cards as rocket if both jokers are played.
+  """
   def get_cat([52, 53]) do
     :rocket
   end
-
+  
+  @doc """
+  Recognize the type of the cards fewer than 5.
+  """
   def get_cat(cards) when length(cards) < 5 do
     if (hd(cards) == List.last(cards)) do
       case length(cards) do
-        1 -> :single
-        2 -> :pair
-        3 -> :trio
-        4 -> :bomb
+        1 -> {:single, 1, hd(cards)}
+        2 -> {:pair, 1, hd(cards)}
+        3 -> {:trio0, 1, hd(cards)}
+        4 -> {:bomb, 1, hd(cards)}
       end
     else
-      if length(cards) == 4 
-        && (Enum.at(cards, 0) == Enum.at(cards, 2) 
-        || Enum.at(cards, 1) == Enum.at(cards, 3)) do
-        :trio1
+      if length(cards) == 4 do
+        cond do
+          Enum.at(cards, 0) == Enum.at(cards, 2) -> 
+            {:trio1, 1, hd(cards)}
+          Enum.at(cards, 1) == Enum.at(cards, 3) ->
+            {:trio1, 1, List.last(cards)}
+          true -> :illegal
+        end
+      else
+        :illegal
       end
     end
   end
   
+  @doc """
+  Recognize the type of cards more or equal than 5
+  """
   def get_cat(cards) do
   	cards
     |> count(%{})
@@ -41,6 +60,7 @@ defmodule Doudizhu.Rule do
     m
   end
   
+  # Count the frequence of each cards, return a map %{card => freq}
   defp count(c, m) do
     [ head | c ] = c
     m = Map.put(m, head, Map.get(m, head, 0) + 1)
@@ -49,25 +69,25 @@ defmodule Doudizhu.Rule do
 
   # trio with 2
   defp cat_helper(%{2 => [_a], 3 => [b]}, 5) do
-  	{:trio2, b}
+  	{:trio2, 1, b}
   end
 
   # four with two individual single tickers
   defp cat_helper(%{1 => _, 4 => [b]}, 6) do
-  	{:four1, b}
+  	{:four1, 1, b}
   end
 
   # four with two individual pair tickers 
   defp cat_helper(%{2 => [_a, _b], 4 => [c]}, 8) do
-  	{:four2, c}
+  	{:four2, 1, c}
   end
 
   # airplane sequence with single kiker, 2,3,4,5
   defp cat_helper(%{1 => a, 3 => b}, l) when rem(l, 4) == 0 do
   	la = length(a)
   	lb = length(b)
-  	if la + lb * 3 == l && la == lb do
-  		{:air, 1, lb, b |> Enum.sort |> hd}
+  	if la + lb * 3 == l && la == lb && Enum.max(b) < 12 do
+  		{:air1, lb, b |> Enum.sort |> hd}
   	else
   		:illegal
   	end
@@ -77,24 +97,24 @@ defmodule Doudizhu.Rule do
   defp cat_helper(%{2 => a, 3 => b}, l) when rem(l, 5) == 0 do
   	la = length(a)
   	lb = length(b)
-  	if la + lb * 3 == l && la == lb do
-  		{:air, 2, lb, b |> Enum.sort |> hd}
+  	if la + lb * 3 == l && la == lb  && Enum.max(b) < 12 do
+  		{:air2, lb, b |> Enum.sort |> hd}
   	else
   		:illegal
   	end
   end
 
 
-  # airplane sequence 2,3,4,5,6 
+  # airplane sequence 2,3,4,5,6 without ticker, :air0
   defp cat_helper(%{3 => a}, l) when rem(l, 3) == 0 do
-  	if length(a) * 3 == l do
-  		{:air, 0, length(a), a |> Enum.sort |> hd}
+  	if length(a) * 3 == l && Enum.max(a) < 12 do
+  		{:air0, length(a), a |> Enum.sort |> hd}
   	else
   		:illegal
   	end
   end
 
-  # TODO: single sequence 5-12 last < 12
+  # single sequence 5-12 last < 12
   defp cat_helper(%{1 => a}, l) do
   	a = Enum.sort(a)
   	if (length(a) == l 
@@ -106,7 +126,7 @@ defmodule Doudizhu.Rule do
   	end
   end
 
-  # TODO: double sequence 3-10, last < 12
+  # double sequence 3-10, last < 12
   defp cat_helper(%{2 => a}, l) do
   	a = Enum.sort(a)
   	if (length(a) * 2 == l 
@@ -118,6 +138,7 @@ defmodule Doudizhu.Rule do
   	end
   end
 
+  # Capture whatever other illegal case
   defp cat_helper(_) do
   	:illegal
   end
--- doudizhu/test/doudizhu/rule_test.exs
+++ doudizhu/test/doudizhu/rule_test.exs
@@ -3,37 +3,97 @@ defmodule Doudizhu.RuleTest do
   import Doudizhu.Rule
   
   test "single" do
-    assert get_cat([51]) == :single
-    assert get_cat([0]) == :single
-    assert get_cat([12]) == :single
-    assert get_cat([5]) == :single
-    assert get_cat([7]) == :single
-    assert get_cat([]) != :single
+    assert get_cat([51]) == {:single, 1, 51}
+    assert get_cat([0]) == {:single, 1, 0}
+    assert get_cat([12]) == {:single, 1, 12}
+    assert get_cat([5]) == {:single, 1, 5}
+    assert get_cat([7]) == {:single, 1, 7}
   end
   
   test "pair" do
-    assert get_cat([5, 5]) == :pair
-    assert get_cat([12, 12]) == :pair
-    assert get_cat([0, 0]) == :pair
-    assert get_cat([1, 0]) != :pair
+    assert get_cat([5, 5]) == {:pair, 1, 5}
+    assert get_cat([12, 12]) == {:pair, 1, 12}
+    assert get_cat([0, 0]) == {:pair, 1, 0}
   end
   
   test "trio" do
-    assert get_cat([5, 5, 5]) == :trio
-    assert get_cat([4,5,5]) != :trio
-    assert get_cat([0, 10,10]) != :trio
-    assert get_cat([0, 0, 0]) == :trio
+    assert get_cat([5, 5, 5]) == {:trio0, 1, 5}
+    assert get_cat([0, 0, 0]) == {:trio0, 1, 0}
+    assert get_cat([12, 12, 12]) == {:trio0, 1, 12}
   end
   
   test "trio1" do
-    assert get_cat([5, 5, 5, 10]) == :trio1
-    assert get_cat([3, 5 ,5,5]) == :trio1
-    assert get_cat([10,10, 10, 10]) != :trio1
-    assert get_cat([0, 0, 1, 1]) != :trio1
+    assert get_cat([5, 5, 5, 10]) == {:trio1, 1, 5}
+    assert get_cat([3, 5 ,5, 5]) == {:trio1, 1, 5}
+  end
+  
+  test "trio2" do
+    assert get_cat([5, 5, 5, 10, 10]) == {:trio2, 1, 5}
+    assert get_cat([3, 3, 5 ,5, 5]) == {:trio2, 1, 5}
+  end
+  
+  test "bomb" do
+    assert get_cat([10, 10, 10, 10]) == {:bomb, 1, 10}
+    assert get_cat([12, 12, 12, 12]) == {:bomb, 1, 12}
+  end
+  
+  test "four1" do
+    assert get_cat([5, 5, 5, 5, 6, 7]) == {:four1, 1, 5}
+    assert get_cat([3, 4, 6, 6 ,6, 6]) == {:four1, 1, 6}
+  end
+  
+  test "four2" do
+    assert get_cat([5, 5, 5, 5, 6, 6, 7, 7]) == {:four2, 1, 5}
+    assert get_cat([3, 3, 4, 4, 6, 6 ,6, 6]) == {:four2, 1, 6}
+  end
+  
+  test "air0" do
+    assert get_cat([7, 7, 7, 8, 8, 8]) == {:air0, 2, 7}
+    assert get_cat([3, 3, 3, 4, 4, 4, 5, 5, 5]) == {:air0, 3, 3}
+    assert get_cat([3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6]) 
+    == {:air0, 4, 3}
+    assert get_cat([7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11]) 
+    == {:air0, 5, 7}
+    assert get_cat(
+        [6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11]
+      ) == {:air0, 6, 6}
+  end
+  
+  test "air1" do
+    assert get_cat([5, 5, 5, 5, 6, 6, 7, 7]) == {:four2, 1, 5}
+    assert get_cat([3, 3, 4, 4, 6, 6 ,6, 6]) == {:four2, 1, 6}
+  end
+  
+  test "air2" do
+    assert get_cat([5, 5, 5, 5, 6, 6, 7, 7]) == {:four2, 1, 5}
+    assert get_cat([3, 3, 4, 4, 6, 6 ,6, 6]) == {:four2, 1, 6}
   end
 
-  test "get_cat for cards larger than 4" do
-    IO.inspect(get_cat([1, 2, 3, 4, 5]))
-    IO.inspect(get_cat([12, 12, 12, 51]))
+  test "single chain" do
+    assert get_cat([1, 2, 3, 4, 5]) == {:schain, 5, 1}
+    assert (0..6 |> Enum.into([]) |> get_cat == {:schain, 7, 0})
+    assert (0..11 |> Enum.into([]) |> get_cat == {:schain, 12, 0})
+    assert get_cat([1, 2, 3, 5, 6]) == :illegal
+    assert (0..12 |> Enum.into([]) |> get_cat == :illegal)
+  end
+  
+  test "pair chain" do
+    assert get_cat([5, 5, 5, 5, 6, 6, 7, 7]) == {:four2, 1, 5}
+    assert get_cat([3, 3, 4, 4, 6, 6 ,6, 6]) == {:four2, 1, 6}
+  end
+  
+  test "rocket" do
+   assert get_cat([52, 53]) == :rocket
+  end
+  
+  test "none" do
+   assert get_cat([]) == :none
+  end
+  
+  test "illegal" do
+   assert get_cat([1, 2]) == :illegal
+   assert get_cat([3, 3, 5]) == :illegal
+   assert get_cat([3, 3, 4, 4]) == :illegal
+   assert get_cat([3, 4, 5, 6]) == :illegal
   end
 end
