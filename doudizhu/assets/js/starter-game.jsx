3f0c0e07affbf7fb0a75660ae8dcfffc2d7427f9
Lingmiao Qiu <qiu.lin@husky.neu.edu>
2019-10-23 01:48:13 -0400

    Add one hand of back card

--- doudizhu/assets/js/starter-game.jsx
+++ doudizhu/assets/js/starter-game.jsx
@@ -14,16 +14,67 @@ class Game extends React.Component {
 				.receive("ok", () => {console.log("ok!")})
 				.receive("error", resp => {console.log("Can't join!", resp)});
 	}
-	
+
+	renderThreeCard(facevalue) {
+		return <Card value={facevalue} />
+	}
+
 	render() {
 		return (
 		<div>
 			<div className="row">
 			<p>Hello!</p>
 			</div>
+
+			<AHandOfCard root={this} />
+
+			<div id="container">
+				<div className="rowScore">
+					<div className="name">Player1</div>
+					<div className="score">430</div>
+				</div>
+
+				<div className="rowScore">
+					<div className="name">Player2</div>
+					<div className="score">580</div>
+				</div>
+
+				<div className="rowScore">
+					<div className="name">Player3</div>
+					<div className="score">310</div>
+				</div>
+			</div>
 		</div>
 		);
 	}
-	
-	
+
+
+	get_view(view) {
+		this.setState(view.game);
+	}
+}
+
+class AHandOfCard extends React.Component {
+	renderCard(i, faceValue) {
+		return <Card key={i} value={faceValue} />
+	}
+	render() {
+		let r = [];
+		for (let i = 0; i < 17; i++) {
+			r.push(this.renderCard(i,"back"))
+		}
+		return (
+			<div className="row">
+				{r}
+			</div>
+		);
+	}
+}
+
+function Card(props) {
+	if (props.value === "back") {
+		return <img src="https://i.ibb.co/b6TDqMS/back.png" width="50" height="100"/>;
+	} else {
+
+	}
 }
