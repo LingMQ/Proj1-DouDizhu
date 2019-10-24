321afd0b56365a849841dd3c05c4d341983549cf
Lingmiao Qiu <qiu.lin@husky.neu.edu>
2019-10-24 13:47:31 -0400

    Resize the card

--- doudizhu/assets/js/card.jsx
+++ doudizhu/assets/js/card.jsx
@@ -1,59 +1,59 @@
 const dict = {
     "back": "https://i.ibb.co/b6TDqMS/back.png",
-    0:  "https://i.ibb.co/CMm1JJg/46.png", // 3
-    1:  "https://i.ibb.co/DrkRpCg/45.png",
-    2:  "https://i.ibb.co/wKq1Bbt/48.png",
-    3:  "https://i.ibb.co/BnxFv18/47.png",
-    4:  "https://i.ibb.co/6JSdzZ0/42.png", // 4
-    5:  "https://i.ibb.co/4VLnqm3/41.png",
-    6:  "https://i.ibb.co/wyKqZcB/44.png",
-    7:  "https://i.ibb.co/G3NQQ9D/43.png",
-    8:  "https://i.ibb.co/Jy9XnHr/39.png", // 5
-    9:  "https://i.ibb.co/3N9VLkk/40.png",
-    10: "https://i.ibb.co/0Y05Q7c/37.png",
-    11: "https://i.ibb.co/1Qq59yf/38.png",
-    12: "https://i.ibb.co/1MNks18/34.png", // 6
-    13: "https://i.ibb.co/F3cnrtQ/33.png",
-    14: "https://i.ibb.co/kMyGZ5T/36.png",
-    15: "https://i.ibb.co/SvPCG81/35.png",
-    16: "https://i.ibb.co/D1638Dn/31.png", // 7
-    17: "https://i.ibb.co/tKGJt2f/32.png",
-    18: "https://i.ibb.co/P1FS8L7/29.png",
-    19: "https://i.ibb.co/BcKDBkC/30.png",
-    20: "https://i.ibb.co/Wc7Qq17/26.png", // 8
-    21: "https://i.ibb.co/J5nPdqp/25.png",
-    22: "https://i.ibb.co/jTBbXqQ/27.png",
-    23: "https://i.ibb.co/09M3tbQ/28.png",
-    24: "https://i.ibb.co/MkLNY0T/24.png", // 9
-    25: "https://i.ibb.co/kBp77nN/23.png",
-    26: "https://i.ibb.co/sKngVfR/22.png",
-    27: "https://i.ibb.co/KzvNJ21/21.png",
-    28: "https://i.ibb.co/WF1csXs/17.png", // 10
-    29: "https://i.ibb.co/5xGBpXV/18.png",
-    30: "https://i.ibb.co/gDk6Byk/19.png",
-    31: "https://i.ibb.co/wW2jMXL/20.png",
-    32: "https://i.ibb.co/cb3XthV/16.png", // 11 J
-    33: "https://i.ibb.co/dkxy7GY/15.png",
-    34: "https://i.ibb.co/5n5kgmB/14.png",
-    35: "https://i.ibb.co/FzpMSZW/13.png",
-    36: "https://i.ibb.co/sp4mjXv/9.png", // 12 Q
-    37: "https://i.ibb.co/GRftg1K/11.png", // *
-    38: "https://i.ibb.co/b64BSJH/10.png",
-    39: "https://i.ibb.co/QdQ4ZPk/12.png",
-    40: "https://i.ibb.co/1M485dB/6.png", // 13 K
-    41: "https://i.ibb.co/P1yPh0V/5.png",
-    42: "https://i.ibb.co/RBBJ4Pm/7.png",
-    43: "https://i.ibb.co/cNRBY63/8.png",
-    44: "https://i.ibb.co/vYBmBcN/2.png", // A
-    45: "https://i.ibb.co/dmrMmM4/3.png",
-    46: "https://i.ibb.co/swXGKBv/4.png",
-    47: "https://i.ibb.co/NYtNbjw/1.png",
-    48: "https://i.ibb.co/k68mp4S/50.png", // 2
-    49: "https://i.ibb.co/vQr9tVn/49.png",
-    50: "https://i.ibb.co/JBHZpBX/52.png",
-    51: "https://i.ibb.co/3cQ9nw7/51.png",
-    52: "https://i.ibb.co/1qpnHZ4/53.png", // little joker
-    53: "https://i.ibb.co/vmQTr7f/54.png", // big joker
+    0:  "https://i.ibb.co/dgr2JBQ/48.jpg", // 3
+    1:  "https://i.ibb.co/dkhHZBZ/47.jpg",
+    2:  "https://i.ibb.co/NVvHMKn/46.jpg",
+    3:  "https://i.ibb.co/PxBTR4j/45.jpg",
+    4:  "https://i.ibb.co/D4kCFqr/44.jpg", // 4
+    5:  "https://i.ibb.co/PFWvX9m/43.jpg",
+    6:  "https://i.ibb.co/mttcL9Y/42.jpg",
+    7:  "https://i.ibb.co/YWwT2Cs/41.jpg",
+    8:  "https://i.ibb.co/Wz9KPt0/40.jpg", // 5
+    9:  "https://i.ibb.co/z29wbC3/39.jpg",
+    10: "https://i.ibb.co/48mGYsR/38.jpg",
+    11: "https://i.ibb.co/n6CDhCk/37.jpg",
+    12: "https://i.ibb.co/V2dtV3q/36.jpg", // 6
+    13: "https://i.ibb.co/FHW3Rrg/35.jpg",
+    14: "https://i.ibb.co/vX475D0/34.jpg",
+    15: "https://i.ibb.co/Jxtxq83/33.jpg",
+    16: "https://i.ibb.co/br2wJSF/32.jpg", // 7
+    17: "https://i.ibb.co/fQKNXhj/31.jpg",
+    18: "https://i.ibb.co/W2bx2Qd/30.jpg",
+    19: "https://i.ibb.co/KK8Trfp/29.jpg",
+    20: "https://i.ibb.co/Y0d4tRm/27.jpg", // 8
+    21: "https://i.ibb.co/M1Lr7xX/26.jpg",
+    22: "https://i.ibb.co/nsc6v73/25.jpg",
+    23: "https://i.ibb.co/8NHxLf2/28.jpg",
+    24: "https://i.ibb.co/Lt0NZYR/24.jpg", // 9
+    25: "https://i.ibb.co/qkj0XY5/23.jpg",
+    26: "https://i.ibb.co/hdqr1Mr/22.jpg",
+    27: "https://i.ibb.co/xjVJ8r0/21.jpg",
+    28: "https://i.ibb.co/LzfX8Zg/17.jpg", // 10
+    29: "https://i.ibb.co/DfjXXhT/20.jpg",
+    30: "https://i.ibb.co/mXSzWzH/19.jpg",
+    31: "https://i.ibb.co/HFNHKFW/18.jpg",
+    32: "https://i.ibb.co/R4GHLr1/16.jpg", // 11 J
+    33: "https://i.ibb.co/RDpvCvr/15.jpg",
+    34: "https://i.ibb.co/fnLd7FB/14.jpg",
+    35: "https://i.ibb.co/tD9GW5F/13.jpg",
+    36: "https://i.ibb.co/Jt3Mrk8/12.jpg", // 12 Q
+    37: "https://i.ibb.co/HXVd0Lp/11.jpg",
+    38: "https://i.ibb.co/jHJBYbS/9.jpg",
+    39: "https://i.ibb.co/3R7zs8q/10.jpg",
+    40: "https://i.ibb.co/W6HQ5dH/8.jpg", // 13 K
+    41: "https://i.ibb.co/Xx8bTL1/7.jpg",
+    42: "https://i.ibb.co/YyRHQBS/5.jpg",
+    43: "https://i.ibb.co/CJyZ4B1/6.jpg",
+    44: "https://i.ibb.co/WkqFTRn/4.jpg", // A
+    45: "https://i.ibb.co/SxhSrTV/3.jpg",
+    46: "https://i.ibb.co/pW9sRVx/1.jpg",
+    47: "https://i.ibb.co/7gSsV3g/2.jpg",
+    48: "https://i.ibb.co/CQTDy3v/52.jpg", // 2
+    49: "https://i.ibb.co/FnhqbD4/51.jpg",
+    50: "https://i.ibb.co/TBNqQnN/50.jpg",
+    51: "https://i.ibb.co/8jQn6jN/49.jpg",
+    52: "https://i.ibb.co/gFmW0FS/53.jpg", // little joker
+    53: "https://i.ibb.co/MZQqbXw/54.jpg", // big joker
 }
 
 export {dict};
