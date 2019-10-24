65bf5af52882c02fb65ed0e525ae36dd592b33a2
sikang hu <sikang_hu@berkeley.edu>
2019-10-24 09:13:47 -0400

    modified react listener

--- doudizhu/assets/js/starter-game.jsx
+++ doudizhu/assets/js/starter-game.jsx
@@ -157,7 +157,7 @@ function AHandOfCard(props) {
 	console.log(props);
 	console.log(h.includes(1));
 	for(let i = 0; i < props.hands.length; i++) {
-		let f = props.selected.includes(props.hands[i]);
+		let f = (props.selected).includes(props.hands[i]);
 		h.push(<Card key={props.hands[i]} 
 			value={props.hands[i]} 
 			select={f} 
