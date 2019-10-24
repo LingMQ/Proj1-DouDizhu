00580a6217156f99c75ce5d7fa8f7ec575820b21
Lingmiao Qiu <qiu.lin@husky.neu.edu>
2019-10-23 23:41:09 -0400

    Another version of front end
    (cherry picked from commit 99f75a6a176ca05319c4a71d0e0ff431e17aa2ef)

---
+++ doudizhu/assets/js/card.jsx
@@ -0,0 +1,59 @@
+const dict = {
+    "back": "https://i.ibb.co/b6TDqMS/back.png",
+    0:  "https://i.ibb.co/CMm1JJg/46.png", // 3
+    1:  "https://i.ibb.co/DrkRpCg/45.png",
+    2:  "https://i.ibb.co/wKq1Bbt/48.png",
+    3:  "https://i.ibb.co/BnxFv18/47.png",
+    4:  "https://i.ibb.co/6JSdzZ0/42.png", // 4
+    5:  "https://i.ibb.co/4VLnqm3/41.png",
+    6:  "https://i.ibb.co/wyKqZcB/44.png",
+    7:  "https://i.ibb.co/G3NQQ9D/43.png",
+    8:  "https://i.ibb.co/Jy9XnHr/39.png", // 5
+    9:  "https://i.ibb.co/3N9VLkk/40.png",
+    10: "https://i.ibb.co/0Y05Q7c/37.png",
+    11: "https://i.ibb.co/1Qq59yf/38.png",
+    12: "https://i.ibb.co/1MNks18/34.png", // 6
+    13: "https://i.ibb.co/F3cnrtQ/33.png",
+    14: "https://i.ibb.co/kMyGZ5T/36.png",
+    15: "https://i.ibb.co/SvPCG81/35.png",
+    16: "https://i.ibb.co/D1638Dn/31.png", // 7
+    17: "https://i.ibb.co/tKGJt2f/32.png",
+    18: "https://i.ibb.co/P1FS8L7/29.png",
+    19: "https://i.ibb.co/BcKDBkC/30.png",
+    20: "https://i.ibb.co/Wc7Qq17/26.png", // 8
+    21: "https://i.ibb.co/J5nPdqp/25.png",
+    22: "https://i.ibb.co/jTBbXqQ/27.png",
+    23: "https://i.ibb.co/09M3tbQ/28.png",
+    24: "https://i.ibb.co/MkLNY0T/24.png", // 9
+    25: "https://i.ibb.co/kBp77nN/23.png",
+    26: "https://i.ibb.co/sKngVfR/22.png",
+    27: "https://i.ibb.co/KzvNJ21/21.png",
+    28: "https://i.ibb.co/WF1csXs/17.png", // 10
+    29: "https://i.ibb.co/5xGBpXV/18.png",
+    30: "https://i.ibb.co/gDk6Byk/19.png",
+    31: "https://i.ibb.co/wW2jMXL/20.png",
+    32: "https://i.ibb.co/cb3XthV/16.png", // 11 J
+    33: "https://i.ibb.co/dkxy7GY/15.png",
+    34: "https://i.ibb.co/5n5kgmB/14.png",
+    35: "https://i.ibb.co/FzpMSZW/13.png",
+    36: "https://i.ibb.co/sp4mjXv/9.png", // 12 Q
+    37: "https://i.ibb.co/GRftg1K/11.png", // *
+    38: "https://i.ibb.co/b64BSJH/10.png",
+    39: "https://i.ibb.co/QdQ4ZPk/12.png",
+    40: "https://i.ibb.co/1M485dB/6.png", // 13 K
+    41: "https://i.ibb.co/P1yPh0V/5.png",
+    42: "https://i.ibb.co/RBBJ4Pm/7.png",
+    43: "https://i.ibb.co/cNRBY63/8.png",
+    44: "https://i.ibb.co/vYBmBcN/2.png", // A
+    45: "https://i.ibb.co/dmrMmM4/3.png",
+    46: "https://i.ibb.co/swXGKBv/4.png",
+    47: "https://i.ibb.co/NYtNbjw/1.png",
+    48: "https://i.ibb.co/k68mp4S/50.png", // 2
+    49: "https://i.ibb.co/vQr9tVn/49.png",
+    50: "https://i.ibb.co/JBHZpBX/52.png",
+    51: "https://i.ibb.co/3cQ9nw7/51.png",
+    52: "https://i.ibb.co/1qpnHZ4/53.png", // little joker
+    53: "https://i.ibb.co/vmQTr7f/54.png", // big joker
+}
+
+export {dict};
--- doudizhu/assets/js/starter-game.jsx
+++ doudizhu/assets/js/starter-game.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
+
 export default function game_init(root, channel) {
 	ReactDOM.render(<Game channel={channel}/>, root);
 }
@@ -13,36 +14,45 @@ class Game extends React.Component {
 		this.channel.join()
 				.receive("ok", () => {console.log("ok!")})
 				.receive("error", resp => {console.log("Can't join!", resp)});
-	}
 
-	renderThreeCard(facevalue) {
-		return <Card value={facevalue} />
+		this.state = {
+			players: 0,
+			hands: [], // this is a 2d array for 3players' hand of card
+			last: [],
+			score: [430, 580, 310],
+		};
 	}
 
 	render() {
 		return (
 		<div>
-			<div className="row">
-			<p>Hello!</p>
+			<h2 align="center">Hello, {window.playerName}! Welcome to Fight Against Landlord game room "{window.gameName}"... </h2>
+
+			<div className="row-firstline">
+				<Scoreboard root={this} />
+				<div className="dizhuCard">
+					<Card value={37} select={false}/>
+					<Card value={37} select={false}/>
+					<Card value={37} select={false}/>
+				</div>
+				<Timer root={this}/>
 			</div>
 
-			<AHandOfCard root={this} />
-
-			<div id="container">
-				<div className="rowScore">
-					<div className="name">Player1</div>
-					<div className="score">430</div>
+			<div className="row">
+				<div className="column" float="left">
+					<OpponentDealCard root={this} />
 				</div>
-
-				<div className="rowScore">
-					<div className="name">Player2</div>
-					<div className="score">580</div>
+				<div className="column" float="right">
+					<OpponentDealCard root={this} />
 				</div>
+			</div>
+			<div className="column">
+				<MyDealCard root={this} />
+			</div>
 
-				<div className="rowScore">
-					<div className="name">Player3</div>
-					<div className="score">310</div>
-				</div>
+			<button className="readyButton" onClick={this.addGamePlayer.bind(this)}> Ready for Game?? </button>
+			<div className="column">
+				<AHandOfCard root={this} />
 			</div>
 		</div>
 		);
@@ -52,16 +62,40 @@ class Game extends React.Component {
 	get_view(view) {
 		this.setState(view.game);
 	}
+
+	addGamePlayer() {
+		//TODO work on the channel side for this ???
+		this.channel.push("addPlayer", {})
+			.receive("ok", this.get_view.bind(this))
+	}
+}
+
+function OpponentDealCard(props) {
+	return (
+		<div>
+			<Card value={37} select={false}/>
+			<Card value={37} select={false}/>
+			<Card value={37} select={false}/>
+		</div>
+	);
+}
+
+function MyDealCard(props) {
+	return <Card value={37} select={false}/>
 }
 
 class AHandOfCard extends React.Component {
-	renderCard(i, faceValue) {
-		return <Card key={i} value={faceValue} />
+	renderCard(faceValue, select) {
+		return <Card value={faceValue} select={select}/>
 	}
 	render() {
 		let r = [];
-		for (let i = 0; i < 17; i++) {
-			r.push(this.renderCard(i,"back"))
+		for (let i = 0; i < 10; i++) {
+			r.push(this.renderCard("back", false))
+		}
+		r.push(<Card value={32} select={true} />)
+		for (let i = 11; i < 20; i++) {
+			r.push(this.renderCard("back", false))
 		}
 		return (
 			<div className="row">
@@ -72,9 +106,45 @@ class AHandOfCard extends React.Component {
 }
 
 function Card(props) {
-	if (props.value === "back") {
-		return <img src="https://i.ibb.co/b6TDqMS/back.png" width="50" height="100"/>;
-	} else {
+	let cardUrl = (require("./card").dict)[props.value];
 
+	if (props.value == "back") {
+		return <img src={cardUrl} width="55" height="100"/>
+	} else {
+		if (props.select) {
+			return <img src={cardUrl} width="85" height="160"/>
+		} else {
+			return <img src={cardUrl} width="58" height="108"/>
+		}
 	}
 }
+
+class Scoreboard extends React.Component{
+	renderOnePlayer(i, score) {
+		let name = "Player" + (i + 1)
+		return (
+			<div className="rowScore">
+				<div className="name"> {name} </div>
+				<div className="score"> {score} </div>
+			</div>
+		)
+	}
+
+	render() {
+		let scoreData = this.props.root.state.score
+		let l = [];
+		for (let i = 0; i < scoreData.length; i++) {
+			l.push(this.renderOnePlayer(i, scoreData[i]))
+		}
+		return (
+			<div id="container">
+				{l}
+			</div>
+		);
+	}
+}
+
+function Timer(props) {
+	return (<div className="time"> 00:00:00 </div>)
+}
+
